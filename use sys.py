58f9410c80d21e0f80ac2dc6fc79c9fe19e417a8
shenliang34 <971935832@qq.com>
2015-01-28 00:08:09 +0800

    first commit

---
+++ use sys.py
@@ -0,0 +1,14 @@
+import sys
+for i in sys.argv:
+	print i
+print '\n\n the python path is',sys.path,'\n'
+i=1
+while i<=10:
+	print i;
+	i=i+1;
+	pass
+j="name"
+mylist=['li','fj']
+print j.startswith('nmm')
+print j.join(mylist)// 
+print j.find('sys')
